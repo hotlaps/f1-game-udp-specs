f0e6878a273bb7790e8fc7f2a83ed4c3d9e7dbaa
Björn Kaiser <bjoern@codegefluester.de>
2022-06-14 12:08:26 +0200

    initial

--- build_diff.sh
+++ build_diff.sh
@@ -2,5 +2,5 @@
 FILES_2022=(`ls -1 2022`)
 
 for ((i=0; i < ${#FILES_2022[@]}; i++)); do
-    git diff "2022/${FILES_2022[$i]} 2021/${FILES_2022[$i]}"
+    git diff "2022/${FILES_2022[$i]}" "2021/${FILES_2022[$i]}" >> "diff_${FILES_2022[$i]}.txt"
 done
