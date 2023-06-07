fe06ffd1438f9627d86023ad445928b03d650216
Bjorn Kaiser <bjoern@codegefluester.de>
2023-06-07 15:21:48 +0200

    Ignore whitespace/tabs when diffing

--- build_diff.sh
+++ build_diff.sh
@@ -4,7 +4,7 @@ FILES_2023=(`ls -1 2023`)
 for ((i=0; i < ${#FILES_2023[@]}; i++)); do
     if test -f "2022/${FILES_2023[$i]}";
     then
-        git diff --no-index "2022/${FILES_2023[$i]}" "2023/${FILES_2023[$i]}" > "diff_2022-2023/${FILES_2023[$i]}.diff"
+        git diff --no-index --ignore-all-space "2022/${FILES_2023[$i]}" "2023/${FILES_2023[$i]}" > "diff_2022-2023/${FILES_2023[$i]}.diff"
         DIFF_SIZE=(`wc -c "diff_2022-2023/${FILES_2023[$i]}.diff" | awk '{print $1}'`)
         if test $DIFF_SIZE -eq 0; 
         then 
